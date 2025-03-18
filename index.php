e4c8194becbfab6faac850696f96d337d77f9d31
rsy64 <shail31y@gmail.com>
2025-03-18 14:43:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,3 +12,32 @@
     <link href="css/styles.css" rel="stylesheet">
 </head>
 <body>
+<?php include 'header.php'; ?>
+
+<div class="container mt-5">
+    <h1 class="text-center">Welcome to the Multi-Tool Website</h1>
+    <div class="row mt-5">
+        <div class="col-md-3">
+            <a href="calculator.php" class="btn btn-primary btn-block">
+                <i class="fas fa-calculator"></i> Calculator
+            </a>
+        </div>
+        <div class="col-md-3">
+            <a href="todo.php" class="btn btn-success btn-block">
+                <i class="fas fa-tasks"></i> To-Do List
+            </a>
+        </div>
+        <div class="col-md-3">
+            <a href="converter.php" class="btn btn-info btn-block">
+                <i class="fas fa-balance-scale"></i> Unit Converter
+            </a>
+        </div>
+        <div class="col-md-3">
+            <a href="password-generator.php" class="btn btn-warning btn-block">
+                <i class="fas fa-key"></i> Password Generator
+            </a>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
