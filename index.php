a01de095996f3d00018762d173d2da9dbbb3368b
rsy64 <shail31y@gmail.com>
2025-03-18 14:10:42 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Welcome to the Multi-Tool Website</h1>
+    <div class="row">
+        <div class="col-md-4 mb-4">
+            <a href="calculator.php" class="btn btn-primary btn-block">
+                <i class="fas fa-calculator"></i> Calculator
+            </a>
+        </div>
+        <div class="col-md-4 mb-4">
+            <a href="todo.php" class="btn btn-success btn-block">
+                <i class="fas fa-tasks"></i> To-Do List
+            </a>
+        </div>
+        <div class="col-md-4 mb-4">
+            <a href="converter.php" class="btn btn-info btn-block">
+                <i class="fas fa-balance-scale"></i> Unit Converter
+            </a>
+        </div>
+        <div class="col-md-4 mb-4">
+            <a href="password-generator.php" class="btn btn-warning btn-block">
+                <i class="fas fa-key"></i> Password Generator
+            </a>
+        </div>
+        <div class="col-md-4 mb-4">
+            <a href="age-calculator.php" class="btn btn-danger btn-block">
+                <i class="fas fa-birthday-cake"></i> Age Calculator
+            </a>
+        </div>
+    </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
