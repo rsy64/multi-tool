7db013e975b9a695ba40456330b1f00f8bc7928e
rsy64 <shail31y@gmail.com>
2025-03-18 14:35:45 +0530

    Delete ads.php
    yes

--- ads.php
+++
@@ -1,7 +0,0 @@
-<div class="mt-4">
-    <h3>Advertisement</h3>
-    <div class="ad-placeholder">
-        <!-- Ad Content Here -->
-        <img src="https://via.placeholder.com/300x250" alt="Ad" class="img-fluid">
-    </div>
-</div>
