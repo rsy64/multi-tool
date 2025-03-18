1e6701827ec0be9357116b91240678238befe757
rsy64 <shail31y@gmail.com>
2025-03-18 14:50:05 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multi-Tool Website</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Multi-Tool Website</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="calculator.php">Calculator</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="todo.php">To-Do List</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="converter.php">Unit Converter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="password-generator.php">Password Generator</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
